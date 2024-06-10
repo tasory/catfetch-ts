290ac2626bcd2cc9133cffd531c82347f4c9dc95
Tasory <83129333+tasory@users.noreply.github.com>
2024-06-11 01:43:06 +0400

    Add files via upload

--- fetch.sh
+++ fetch.sh
@@ -2,23 +2,29 @@
 
 # catfetch
 
+# main varibles
 kr=$(uname -r)
 wm=$(echo $XDG_CURRENT_DESKTOP)
 du=$(uptime -p)
 up=${du:2}
 dp=$(lscpu | grep "Model name:")
 cp=${dp:14}
-hn=$(cat /etc/hostname)
+hn=$(hostname)
 mu=$(playerctl metadata --format "{{artist}} - {{title}}")
 
-echo	"⠀⠀⠀⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ " ────── $hn ───────
-echo	"⠀⠀⠀⠀⣠⠎⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ " "|" 
-echo	"⢀⣀⡀⠞⠁⠀⠀⠁⠁⠐⠒⠒⠋⡹⠀⠀⠀⠀ " "|->" up: $up
-echo	"⠈⢢⠀⠾⠃⠀⠀⠀⠀⠀⠀⠀⡰⠁⠀⠀⠀⠀ " "|"
-echo	"⢰⠣⡄⠠⢄⠀⠾⠅⠀⠀⢠⣰⠁⠀⠀⠀⠀⠀ " "|->" kr: $kr 
-echo	"⠀⠀⠐⢤⣀⠀⣀⣆⢰⠉⠢⡇⠀⢀⠤⠤⣀⠀ " "|"
-echo	"⠀⠀⢸⠀⠀⠀⠀⠀⠙⠀⠈⠳⡀⠘⠤⡀⠀⢆ " "|->" cp: $cp
-echo	"⠀⠀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢄⡀⢸⠀⠸ " "|"
-echo	"⠀⠀⠁⠀⢥⠀⠀⡇⠀⠀⠀⠀⠀⠀⣱⠼⠀⡘ " "|->" wm: $wm
-echo	"⠀⢀⡇⠀⠢⠄⢀⠃⠀⢐⠀⠀⠀⠀⡇⠤⠋⠀ " "|"
-echo	"⠀⠸⢄⣀⠜⠀⢸⡀⠴⠋⠀⠉⠁⠁⠀⠀⠀⠀ " "|->" sg: $mu
+# colors
+white="\e[97m"
+gray="\e[90m"
+def="\e[0m"
+
+echo -e "${gray} ⠀⠀⠀⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ${def}" ┌───── $hn 
+echo -e "${gray} ⠀⠀⠀⠀⣠⠎⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ${def}" "│"
+echo -e "${gray} ⢀⣀⡀⠞⠁⠀⠀⠁⠁⠐⠒⠒⠋⡹⠀⠀⠀⠀ ${def}" "├─" 󱑃 ${gray} $up ${def}
+echo -e "${gray} ⠈⢢${white}⠀⠾⠃⠀⠀⠀⠀⠀⠀${gray}⠀⡰⠁⠀⠀⠀⠀ ${def}" "│"
+echo -e "${gray} ⢰⠣⡄⠠⢄⠀${white}⠾⠅   ${gray}⢠⣰⠁⠀⠀⠀⠀⠀${def}" "├─"  ${gray} $kr ${def}
+echo -e "${gray} ⠀⠀⠐⢤⣀⠀⣀⣆⢰⠉⠢⡇⠀⢀⠤⠤⣀⠀ ${def}" "│"
+echo -e "${gray} ⠀⠀⢸⠀⠀⠀⠀⠀⠙⠀⠈⠳⡀⠘⠤⡀⠀⢆ ${def}" "├─"  ${gray} $cp ${def}
+echo -e "${gray} ⠀⠀⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢄⡀⢸⠀⠸ ${def}" "│"
+echo -e "${gray} ⠀⠀⠁⠀⢥⠀⠀⡇⠀⠀⠀⠀⠀⠀⣱⠼⠀⡘ ${def}" "├─"  ${gray} $wm ${def}
+echo -e "${gray} ⠀⢀⡇⠀⠢⠄⢀⠃⠀⢐⠀⠀⠀⠀⡇⠤⠋⠀ ${def}" "│"
+echo -e "${gray}⠀ ⠸⢄⣀⠜⠀⢸⡀⠴⠋⠀⠉⠁⠁⠀⠀⠀⠀ ${def}" "└─" 󰝚 ${gray} $mu ${def}
